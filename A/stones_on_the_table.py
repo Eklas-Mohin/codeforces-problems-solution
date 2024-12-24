1137a0a78afa870e030f8b0ee7893267699f0ff6
Eklas-Mohin <mohin030299@gmail.com>
2024-12-24 11:55:35 +0600

    Codeforces 266A. Stones on the Table

---
+++ A/stones_on_the_table.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input())
+    s = input()
+    ans = 0
+
+    for i in range(1, n):
+        if s[i] == s[i - 1]:
+            ans += 1
+            
+    print(ans)
+    
+if __name__ == '__main__':
+    main()
