f6b191f6de5062998882f9cb02f9545115091396
Eklas-Mohin <mohin030299@gmail.com>
2024-12-24 12:08:08 +0600

    Codeforces 236A. Boy or Girl

---
+++ A/boy_or_girl.py
@@ -0,0 +1,14 @@
+def main():
+    s = input()
+    st = set()
+
+    for c in s:
+        st.add(c)
+        
+    if len(st) % 2 == 0:
+        print('CHAT WITH HER!')
+    else:
+        print('IGNORE HIM!')
+
+if __name__ == '__main__':
+    main()
