75d05a41b4d32ea67a20a054a5ed86169345ebaf
Eklas-Mohin <mohin030299@gmail.com>
2024-12-24 23:18:41 +0600

    Codeforces 272A. Dima and Friends

--- A/dima_and_friends.py
+++ A/dima_and_friends.py
@@ -2,11 +2,11 @@ def main():
     n = int(input())
     k = list(map(int, input().split()))
     s = sum(k)
-    ans = 5
+    ans = 0
 
     for i in range(s + 1, s + 6):
-        if (i - 1) % (n + 1) == 0:
-            ans -= 1
+        if (i - 1) % (n + 1) != 0:
+            ans += 1
 
     print(ans)
     
