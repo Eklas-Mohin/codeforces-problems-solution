9148c072ec209407c7cae0449238598f8ea05b96
Eklas-Mohin <mohin030299@gmail.com>
2025-01-10 12:35:51 +0600

    Codeforces 791A. Bear and Big Brother

---
+++ A/bear_and_big_brother.py
@@ -0,0 +1,13 @@
+def main():
+    a, b = map(int, input().split())
+    ans = 0
+
+    while a <= b:
+        a = a * 3
+        b = b * 2
+        ans += 1
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
