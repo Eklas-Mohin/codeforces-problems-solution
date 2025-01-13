95d232c73bac72f0ab27b594f6dbe9dfc269db64
Eklas-Mohin <mohin030299@gmail.com>
2025-01-13 12:28:21 +0600

    Codeforces 1742A. Sum

---
+++ A/sum.py
@@ -0,0 +1,13 @@
+def main():
+    t = int(input())
+
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        arr.sort()
+        if arr[0] + arr[1] == arr[2]:
+            print('YES')
+        else:
+            print('NO')
+
+if __name__ == '__main__':
+    main()
